6dde5312e967fe3866058b83dda1d8dd2b5d3edc
identityofsine <identityofsine@gmail.com>
2024-12-28 01:16:10 -0500

    added new navigation buttons

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -2,6 +2,7 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import './styles/navigation.scss';
 import React, { ReactNode, useEffect, useState } from 'react';
 import { Authentication } from '../api/auth';
+import { assets } from '..';
 
 function Link(props: { href: string; children: ReactNode }) {
 	const router = useNavigate();
@@ -11,6 +12,20 @@ function Link(props: { href: string; children: ReactNode }) {
 	}
 
 	return <div onClick={() => navigate(props.href)}>{props.children}</div>;
+}
+
+function LinkImage(props: { href: string; src: string; alt: string, on: boolean }) {
+	const router = useNavigate();
+
+	function navigate(path: string) {
+		router(path);
+	}
+
+	return (
+		<div className={`navigation__row__buttons__button ${props.on && 'active'}`} onClick={() => navigate(props.href)} >
+			<img src={props.src} alt={props.alt} />
+		</div>
+	);
 }
 
 function Navigator() {
@@ -47,6 +62,7 @@ function Navigator() {
 		// code to update the current spot based on location
 		if (ref.current) {
 			let spot = 0;
+			let disable = false;
 			switch (current) {
 				case '/login':
 				case '/':
@@ -58,98 +74,80 @@ function Navigator() {
 				case '/search':
 					spot = 2;
 					break;
-				case '/profile':
-					spot = 3;
-					break;
-				case '/settings':
-					spot = 4;
-					break;
 				default:
-					spot = 0;
+					spot = -1;
+					disable = true;
 					break;
 			}
 			ref.current.style.setProperty('--spot', `${spot}`);
+			ref.current.style.setProperty('--disable', disable ? '0' : '1');
 		}
 	}, [current]);
 
-	if (Authentication.getLoggedIn()) {
-		return (
-			<nav className="navigation">
-				<div className="navigation__row">
-					<div className="netscape-box effect" ref={ref} />
-					<div className="netscape-box effect-2" ref={hover} />
-					<Link href="/">
-						<div
-							className="netscape-box"
-							datatype-order={0}
-							onMouseEnter={handleHover}
-							onMouseLeave={handleExit}
-						>
-							<span>Home</span>
-						</div>
-					</Link>
-					<div className="netscape-box disabled" datatype-order={1}>
-						<span>N/A</span>
-					</div>
-
-					<Link href="/search">
-						<div className="netscape-box"
-							datatype-order={2}
-							onMouseEnter={handleHover}
-							onMouseLeave={handleExit}
-						>
-							<span>Search</span>
-						</div>
-					</Link>
-					<Link href="/profile">
-						<div
-							className="netscape-box "
-							datatype-order={3}
-							onMouseEnter={handleHover}
-							onMouseLeave={handleExit}
-						>
-							<span>Profile</span>
-						</div>
-					</Link>
-					<Link href="/settings">
-						<div
-							className="netscape-box "
-							datatype-order={4}
-							onMouseEnter={handleHover}
-							onMouseLeave={handleExit}
-						>
-							<span>Settings</span>
-						</div>
-					</Link>
-				</div>
-			</nav>
-		);
-	}
 	return (
 		<nav className="navigation">
 			<div className="navigation__row">
 				<div className="netscape-box effect" ref={ref} />
 				<div className="netscape-box effect-2" ref={hover} />
-				<Link href="/login">
-					<div
-						className="netscape-box"
-						datatype-order={0}
-						onMouseEnter={handleHover}
-						onMouseLeave={handleExit}
-					>
-						<span>Login</span>
-					</div>
-				</Link>
-				<Link href="/signup">
-					<div
-						className="netscape-box"
-						datatype-order={1}
-						onMouseEnter={handleHover}
-						onMouseLeave={handleExit}
-					>
-						<span>Sign-Up</span>
-					</div>
-				</Link>
+				{Authentication.getLoggedIn() ? (
+					<>
+						<Link href="/">
+							<div
+								className="netscape-box"
+								datatype-order={0}
+								onMouseEnter={handleHover}
+								onMouseLeave={handleExit}
+							>
+								<span>Home</span>
+							</div>
+						</Link>
+						<div className="netscape-box disabled" datatype-order={1}>
+							<span>N/A</span>
+						</div>
+						<Link href="/search">
+							<div className="netscape-box"
+								datatype-order={2}
+								onMouseEnter={handleHover}
+								onMouseLeave={handleExit}
+							>
+								<span>Search</span>
+							</div>
+						</Link>
+
+
+					</>
+				) :
+					(
+						<>
+							<Link href="/login">
+								<div
+									className="netscape-box"
+									datatype-order={0}
+									onMouseEnter={handleHover}
+									onMouseLeave={handleExit}
+								>
+									<span>Login</span>
+								</div>
+							</Link>
+							<Link href="/signup">
+								<div
+									className="netscape-box"
+									datatype-order={1}
+									onMouseEnter={handleHover}
+									onMouseLeave={handleExit}
+								>
+									<span>Sign-Up</span>
+								</div>
+							</Link>
+						</>
+					)}
+			</div>
+			<div className="navigation__row__buttons">
+				<LinkImage href="/settings" src={assets.getAsset('ui/gear')} alt="home" on={current === '/settings'} />
+				{Authentication.getLoggedIn() && (
+					<LinkImage href="/profile" src={assets.getAsset('ui/user')} alt="home" on={current === '/profile'} />
+				)
+				}
 			</div>
 		</nav>
 	);
