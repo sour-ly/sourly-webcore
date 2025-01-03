d76a453cf09e927b8bb17cdbe98f0b68b7ab1ea1
identityofsine <identityofsine@gmail.com>
2025-01-03 17:16:09 -0500

    adding better time strings

--- src/util/sqltime.ts
+++ src/util/sqltime.ts
@@ -7,18 +7,40 @@ export function sqltimetojsdate(sqltime: string) {
 }
 
 export function getFormattedDate(date: Date): string {
-	//if less than 24 hours, show timestamp
-	if (Date.now() - date.getTime() < 86400000) {
+	//if less than 12 hours, show timestamp
+	//
+	const dTime = Date.now() - date.getTime();
+
+	//less than a hour
+	if (dTime < 60 * 1000 * 60) {
+		//less than a minute
+		if (dTime < 60 * 1000) {
+			return 'Just now';
+		}
+		//return x minute(s) ago
+		const diff = Math.floor((Date.now() - date.getTime()) / 1000 / 60);
+		return diff.toString() + ' minute(s) ago';
+	}
+	else if (dTime < 60 * 60 * 12 * 1000) {
+		//return x hour(s) ago
+		const diff = Math.floor((Date.now() - date.getTime()) / 1000 / 60 / 60);
+		return diff.toString() + ' hour(s) ago';
+	} else if (Date.now() - date.getTime() > 60 * 60 * 12 * 1000) {
 		//HH:MM
 		return date.getHours() + ':' + date.getMinutes();
 	}
 	//if less than a week, show day of the week
-	if (Date.now() - date.getTime() < 604800000) {
+	if (dTime < 604800000) {
 		//Day
-		return date.getDay().toString();
+		//less than two days
+		if (dTime < 60 * 60 * 24 * 1 * 1000) {
+			//Yesterday
+			return 'Yesterday';
+		}
+		return date.getDay().toString() + ' days ago';
 	}
 	//if less than a year, show month and day
-	if (Date.now() - date.getTime() < 31556952000) {
+	if (dTime < 31556952000) {
 		//MM/DD
 		return date.getMonth() + '/' + date.getDate();
 	}
